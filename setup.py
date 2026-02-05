d53d6be95c3f3192f2392b4ae3e169fb6c8357ba
Matteo Gamboz <gamboz@medialab.sissa.it>
2026-02-05 11:53:44 +0100

    Release 1.1.4
    - Allow for metadata to be loaded piece-meal

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = f"""
 
 setup(
     name="scienzaexpress.preflights",
-    version="1.1.3",
+    version="1.1.4",
     description="A collection of preflight-check actions for RISE",
     long_description=long_description,
     long_description_content_type="text/markdown",
